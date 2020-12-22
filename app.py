655e56fa32f802e654d5cbca509f447143f5912c
martin <martin-cleven@gmx.net>
2020-12-22 17:09:12 +0100

    ci/cd pipeline using github actions

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
+import flask
 from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 
@@ -12,8 +13,10 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css', dbc.themes
 
 df = get_data()
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-server = app.server
+server = flask.Flask(__name__)
+app = dash.Dash(__name__, server=server, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app.config.suppress_callback_exceptions = True
+
 
 app.layout = html.Div([
     dcc.Tabs(id='tabs-corona', value='time_series', children=[
@@ -30,5 +33,8 @@ app.layout = html.Div([
 ])
 
 
-if __name__ == '__main__':
-    app.run_server(debug=False)
+if __name__ == "__main__":
+    import os
+
+    # debug = False if os.environ.get("DASH_DEBUG_MODE", 'False') == "False" else True
+    app.run_server(host="0.0.0.0", port=8050, debug=False)
